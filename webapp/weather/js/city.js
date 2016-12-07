48a6ad1508c50811e3eb12c35b589657f60ab601
sjingye <sjingye@163.com>
2016-12-07 23:16:51 +0800

    单页面应用，删除旧的文件

--- webapp/weather/js/city.js
+++
@@ -1,9 +0,0 @@
-function renderCity(d) {
-    var str = "<li><em>"+d[0]+"</em><span>当前位置</span></li>";
-    for(var i=1;i<d.length;i++){
-        str += "<li><em>"+d[i]+"</em></li>";
-    }
-    $("ul").html(str);
-}
-renderCity(d);
-//根据选择了的城市渲染
